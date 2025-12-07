647cc03163ab68fd7096db565db708cc0c33cb2f
StasSytnykov <stassytnykov@gmail.com>
2025-12-07 19:54:22 +0200

    fix: AI fixes

--- task.sql
+++ task.sql
@@ -1,16 +1,16 @@
-DROP DATABASE ShopDB;
-
+DROP DATABASE IF EXISTS ShopDB;
 USE ShopDB;
 
 -- Create your stored procedure here
+DROP PROCEDURE IF EXISTS get_warehouse_product_inventory;
 DELIMITER //
 CREATE PROCEDURE get_warehouse_product_inventory(
-    IN ID INT
+    IN p_WarehouseID INT
 )
 BEGIN
-    SELECT Name, WarehouseAmount FROM ProductInventory
+    SELECT Products.Name AS ProductName, ProductInventory.WarehouseAmount AS Amount FROM ProductInventory
     LEFT JOIN Products
     ON ProductInventory.ProductID = Products.ID
-    WHERE ProductInventory.WarehouseID = ID;
+    WHERE ProductInventory.WarehouseID = p_WarehouseID;
 END //
 DELIMITER ;
